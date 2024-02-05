f9ba1a1c46d7ba86718d3a90e3b6d25dfd08363e
Subrata Mondal <subratasubha2@gmail.com>
2024-02-05 12:13:31 +0530

    List Comprehension, Generators

--- concepts/01-generators.py
+++ concepts/01-generators.py
@@ -10,3 +10,14 @@ print(list(even_generator))
 print(
     list(even_generator)
 )  # This will be empty, because generators can only be used once
+
+
+def generate_even_numbers(max):
+    for i in range(0, max + 1):
+        if i % 2 == 0:
+            yield i
+    print("Generator Exhausted Outer")
+
+
+even = list(generate_even_numbers(10))
+print(even)
